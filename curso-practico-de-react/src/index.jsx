1a0560254a9823cf56d3212b5d4a703371c51f2a
IgnaDev <arcortoon@gmail.com>
2022-11-12 15:10:12 -0300

    Vamos a adaptar todos los componentes ya creados a react

--- curso-practico-de-react/src/index.jsx
+++ curso-practico-de-react/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Login from './containers/Login'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Login />, document.getElementById('app'));
