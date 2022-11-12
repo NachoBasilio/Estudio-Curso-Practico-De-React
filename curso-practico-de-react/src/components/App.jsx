f17eb02116e895c049318d7bc44e39e2ac2c2d8b
IgnaDev <arcortoon@gmail.com>
2022-11-12 15:00:15 -0300

    Acomode todo para que funcione con Vite

---
+++ curso-practico-de-react/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+	return (
+		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+	);
+}
+
+export default App;
--- curso-practico-de-react/src/index.jsx
+++ curso-practico-de-react/src/index.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.render(<App />, document.getElementById('app'));
