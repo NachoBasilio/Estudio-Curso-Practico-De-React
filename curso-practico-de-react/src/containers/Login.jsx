96106066ebacda94703d96c91d96e490c184d146
IgnaDev <arcortoon@gmail.com>
2022-11-14 12:05:51 -0300

    No se porque no se muestra el logoooo

--- curso-practico-de-react/src/containers/Login.jsx
+++ curso-practico-de-react/src/containers/Login.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import logo from "../assets/favicon_yard_sale.svg"
 import "../styles/Login.css"
 
 const Login = () => {
 	return (
 		<div className="login">
 			<div className="form-container">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="logo" />
 				<h1 className="title">Create a new password</h1>
 				<p className="subtitle">Enter a new passwrd for yue account</p>
 				<form action="/" className="form">
-					<label for="password" className="label">Password</label>
+					<label htmlFor="password" className="label">Password</label>
 					<input type="password" id="password" placeholder="*********" className="input input-password" />
-					<label for="new-password" className="label">Password</label>
+					<label htmlFor="new-password" className="label">Password</label>
 					<input type="password" id="new-password" placeholder="*********" className="input input-password" />
 					<input type="submit" value="Confirm" className="primary-button login-button" />
       			</form>
