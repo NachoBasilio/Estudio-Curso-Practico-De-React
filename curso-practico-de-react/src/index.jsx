2d07d81674925883704addf91d6dae5a5bcc873e
IgnaDev <arcortoon@gmail.com>
2022-11-15 16:57:03 -0300

    Actualice a react 18

--- curso-practico-de-react/src/index.jsx
+++ curso-practico-de-react/src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import {render} from 'react-dom';
+import App from './routes/App';
 
 
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
